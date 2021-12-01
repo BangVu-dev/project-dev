58dcb2831ab6910602ce12b559a9621e2c66d0b3
Bangvu-dev <bangvu16102001@gmail.com>
2021-12-01 12:25:59 +0700

    dsa

--- src/app/models/KhachHang.js
+++ src/app/models/KhachHang.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+var current = new Date();
+const timeStamp = new Date(Date.UTC(current.getFullYear(), 
+current.getMonth(),current.getDate(),current.getHours(), 
+current.getMinutes(),current.getSeconds(), current.getMilliseconds()));
 const Schema = mongoose.Schema;
 
 const KhachHang = new Schema({
@@ -9,11 +13,10 @@ const KhachHang = new Schema({
     phoneNumber: { type: String },
     quantity: { type: Number },
     payMethod: { type: String },
-    payMethod: { type: String },
     status: { type: Boolean, default: false }, 
     condition: { type: Boolean, default: false },
-}, {
-    timestamps: true,   
+    createdAt: { type: Date, default: timeStamp },
+    updatedAt: { type: Date, default: timeStamp }
 });
 
 module.exports = mongoose.model('KhachHang', KhachHang);
